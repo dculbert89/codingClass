f91696c2f48d7754ca0972ed7b3fc415a7709c19
dculbert89 <dculbert89@gmail.com>
2023-05-17 15:54:53 -0400

    frogger code

---
+++ week4/may17/review.js
@@ -0,0 +1,35 @@
+// const expenses = {
+//     gas: 1000,
+//     rent: 3000,
+//     food: 500,
+//     // paidRent: true,
+//     // payRent: function (){
+//     //     console.log("i paid rent")
+//     // },
+// };
+
+
+// function expenseCalculator(obj) {
+//     let totalExpensses = 0
+//     for(const key in obj){
+//         const value = obj[key]
+//         totalExpensses += value
+//         // totalExpensses = totalExpensses + value;
+//         console.log(totalExpensses)
+//     }
+
+//     obj.total = totalExpensses
+
+// }
+
+// console.log("before function", expenses)
+// expenseCalculator(expenses);
+// console.log("after function", expenses)
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+// const arrayOfChar = str.split(", ", 2);
+// const arrayOfWords = str.split(" ");
+// console.log("array of char", arrayOfChar);
+// // console.log("array of words", arrayOfWords)
+
+
